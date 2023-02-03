8083e029367444ec586bf5cd8be27347f319c4d5
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-03 08:13:58 +0300

    Added changes

--- ITMO.SoftwareEng2023.C++/Pract01_ex1.cpp
+++ ITMO.SoftwareEng2023.C++/Pract01_ex1.cpp
@@ -1,16 +1,12 @@
-// ITMO.SoftwareEng2023.C++
-// Практическое занятие 1. Организация ввода-вывода данных
-// Упражнение 1. Ввод-вывод данных
+// ITMO.SoftwareEng2023.C++. Практическое занятие 1. Организация ввода-вывода данных. Упражнение 1. Ввод-вывод данных
+//
 
 #include <iostream>
 #include <string>
-#include <windows.h>
 using namespace std;
 
 int main()
 {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
 	string name;
 	cout << "What is your name? \n";
 	getline(cin, name);
