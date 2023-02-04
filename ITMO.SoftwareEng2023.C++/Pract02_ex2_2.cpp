8f8b637bdb710efb013f837cc2c7c5f801e5a773
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-04 15:56:30 +0300

    Added Pract02_ex2_2

---
+++ ITMO.SoftwareEng2023.C++/Pract02_ex2_2.cpp
@@ -0,0 +1,27 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 2. Реализация управляющих операторов
+// Упражнение 2. Использование циклов при реализации алгоритмов
+// Задание 2. Использование цикла с предусловием
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	int a, b, temp;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	while (a != b) // алгоритм Евклида
+	{
+		if (a > b)
+			a -= b;
+		else
+			b -= a;
+	}
+
+	cout << "НОД = " << a << endl;
+}
