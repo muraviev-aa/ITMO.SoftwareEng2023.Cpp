94ddd3d9a141d42955e33db165cb628cde6e89e9
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-04 18:58:09 +0300

    Added Pract02_ex4

---
+++ ITMO.SoftwareEng2023.C++/Pract02_ex4.cpp
@@ -0,0 +1,26 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 2. Реализация управляющих операторов
+// Упражнение 4. Расчет суммы чисел на заданном интервале
+//
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	int k, m;
+	int s = 0;
+	cout << "Введите k = "; cin >> k;
+	cout << "Введите m = "; cin >> m;
+
+	for (int i = 1;i <= 100;i++)
+	{
+		if ((i > k) && (i < m))
+			continue;
+		s += i;
+	}
+
+	cout << "Сумма чисел в диапазоне от 1 до k и от m до 100 равна: " << s << endl;
+	return 0;
+}
