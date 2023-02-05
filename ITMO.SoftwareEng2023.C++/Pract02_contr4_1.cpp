bf42e0bb29bef018299a962eba3d9897ee74e450
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-05 15:13:28 +0300

    Added Pract02_contr4_1

---
+++ ITMO.SoftwareEng2023.C++/Pract02_contr4_1.cpp
@@ -0,0 +1,67 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 2. Контрольные задания. 
+// Задание 4. Стрельба по мишени. Часть 1.
+// Мишень - вариант 2.
+//
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	int s;          // количество выстрелов
+	int summa = 0;   // сумма очков
+	double x, y;     // координаты выстрела
+	double r;        // радиус сектора
+
+	cout << "Введите количество выстрелов "; cin >> s;
+
+	while (s > 0)
+	{
+		cout << "Введите координаты x и y выстрела по мишени через пробел\n";
+		cin >> x >> y;
+		r = sqrt(pow(x, 2) + pow(y, 2));
+		//cout << "Радиус: " << r << endl;
+
+		if (r <= 3 && r > 2)
+		{
+			summa += 1;
+			s--;
+		}
+		else if(r <= 2 && r > 1)
+		{
+			summa += 5;
+			s--;
+		}
+		else if (r <= 1)
+		{
+			summa += 10;
+			s--;
+		}
+		else
+		{
+			summa = 0;
+			s--;
+		}
+	}
+
+	cout << "Количество очков: " << summa << endl;
+
+	if (summa < 5)
+	{
+		cout << "Уровень: новичок." << endl;
+	}
+	else if (summa >= 5 && summa < 10)
+	{
+		cout << "Уровень: просто стрелок." << endl;
+	}
+	else if (summa >= 10)
+	{
+		cout << "Уровень: снайпер." << endl;
+	}
+	return 0;
+	
+
+	
+}
