577f3d8e7627f955adcd68a3dfa46d404a02886a
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-06 08:07:38 +0300

    Added Pract03_ex1_2

---
+++ ITMO.SoftwareEng2023.C++/Pract03_ex1_2.cpp
@@ -0,0 +1,29 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 3. Использование функций 
+// Упражнение 1. Использование функции при организации программы
+// Задание 2. Реализация возвращения значения функции
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+string privet(string name)
+{
+	string str = name + ", " + "hello!\n";
+	return str;
+}
+
+int main()
+{
+	string name;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	string nameOut = privet(name);
+	cout << nameOut << endl;
+	return 0;
+}
+
