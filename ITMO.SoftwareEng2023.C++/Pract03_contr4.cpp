28210420bc9a2bf4583a8947426beaa512a2ad77
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-07 09:07:33 +0300

    Added changes

---
+++ ITMO.SoftwareEng2023.C++/Pract03_contr4.cpp
@@ -0,0 +1,17 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 3. Использование функций
+// Контрольные задания.
+// Задание 4. Рекурсивная функция суммы ряда
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	int a;
+
+}
