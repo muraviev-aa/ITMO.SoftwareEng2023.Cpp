45f50b4a563300bb9e6ad95ccf2992c145d5321a
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-08 09:10:45 +0300

    Added Pract04_ex2

--- ITMO.SoftwareEng2023.C++/Pract04_ex2.cpp
+++ ITMO.SoftwareEng2023.C++/Pract04_ex2.cpp
@@ -8,16 +8,21 @@ using namespace std;
 
 void swap(int*, int*);
 void swap(int&, int&);
+void print(int x, int y);
 
 int main()
 {
-	system("chcp 1251");
 	int x = 5, y = 10;
-	swap(&x, &y);
-	swap(x, y);
+	print(x, y);
+
+	swap(&x, &y);  // Передача в функцию параметра указателя
+	print(x, y);
+
+	swap(x, y);    // Передача в функцию параметра ссылки
+	print(x, y);
 }
 
-void swap(int* x, int* y)
+void swap(int *x, int *y)
 {
 	int temp;
 	temp = *x;
@@ -25,10 +30,15 @@ void swap(int* x, int* y)
 	*y = temp;
 }
 
-void swap(int& x, int& y)
+void swap(int &x, int &y)
 {
 	int temp;
 	temp = x;
 	x = y;
 	y = temp;
+}
+
+void print(int x, int y)
+{
+	cout << "x = " << x << "; y = " << y << endl;
 }
