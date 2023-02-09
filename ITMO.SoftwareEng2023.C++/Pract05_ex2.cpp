4961e919639a744a1fced42c900f7f58413151b8
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-09 09:35:55 +0300

    Added Pract05_ex2

---
+++ ITMO.SoftwareEng2023.C++/Pract05_ex2.cpp
@@ -0,0 +1,34 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 5. Работа с массивами
+// Упражнение 2. Сортировка массива
+// Алгоритм сортировки элементов массива – сортировка выбором
+//
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	const int N = 10;
+	int a[N] = { 1, 25, 6, 32, 43, 5, 96, 23, 4, 55 };
+	int min = 0;
+	int buf = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		min = i;
+
+		for (int j = i + 1; j < N; j++)
+			min = (a[j] < a[min]) ? j : min;
+
+		if (i != min)
+		{
+			buf = a[i];
+			a[i] = a[min];
+			a[min] = buf;
+		}
+	}
+
+	for (int i : a)
+		cout << i << '\t';
+}
