afe8c3bb351028864d2eb1f800cf48fb0b777898
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-10 19:09:18 +0300

    Added Pract05_ex5_1

---
+++ ITMO.SoftwareEng2023.C++/Pract05_ex5_1.cpp
@@ -0,0 +1,44 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 5. Работа с массивами
+// Упражнение 5. Применение структур данных для хранения элементов
+// Задание 1. Сохранение результатов в массиве
+//
+
+#include <iostream> 
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int a, b, c;
+	int k = 0;
+	const int n = 10;
+	int mas[n];
+
+	for (int i = 0;i < n;i++)
+	{
+		// инициализация операндов случайными числами от 1 до 101
+		a = rand() % 10 + 1;
+		b = rand() % 10 + 1;
+		cout << a << " * " << b << " = ";
+		cin >> c;
+		mas[i] = c;
+
+		if (a * b != c)
+		{
+			k++; // операция «инкремент», аналогично: k = k + 1
+			cout << "Error! ";
+			cout << a << " * " << b << " = " << a * b << endl;
+		}
+	}
+
+	cout << "\nAll: "; 
+	for (int i = 0; i < n; i++) 
+	{ 
+		cout << mas[i] << ends; 
+	}
+
+	cout << "Count error: " << k << endl;
+	return 0;
+}
