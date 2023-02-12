b581efe153b4d2d6e55c4656690c0b2177192f55
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-12 13:32:26 +0300

    Added changes

--- ITMO.SoftwareEng2023.C++/Pract05_contr2.cpp
+++ ITMO.SoftwareEng2023.C++/Pract05_contr2.cpp
@@ -7,9 +7,25 @@
 #include <iostream> 
 using namespace std;
 
-int* max_vect(int kc, int a[], int b[])  // указатель на массив, создаваемый в этой функции
+int* max_vect(int kc, int a[], int b[]);
+
+
+int main()
 {
-	int *c = new int[kc];
+	int a[] = { 1,2,3,4,5,6,7,2 };
+	int b[] = { 7,6,5,4,3,2,1,3 };
+	int kc = sizeof(a) / sizeof(a[0]);     // Количество элементов массива
+	int* c;                                // Указатель на результирующий массив
+	c = max_vect(kc, a, b);                // Вызов функции для создания массива
+	for (int i = 0;i < kc; i++)            // Вывод результата.
+		cout << c[i] << " ";
+	cout << endl;
+	delete[]c;                             // Возврат памяти.
+}
+
+int* max_vect(int kc, int a[], int b[])
+{
+	int* c = new int[kc];
 	int i = 0;
 
 	while (kc >= 0)
@@ -28,17 +44,4 @@ int* max_vect(int kc, int a[], int b[])  // указатель на массив
 		kc--;
 	}
 	return c;
-}
-
-int main()
-{
-	int a[] = { 1,2,3,4,5,6,7,2 };
-	int b[] = { 7,6,5,4,3,2,1,3 };
-	int kc = sizeof(a) / sizeof(a[0]);     // Количество элементов массива
-	int* c;                                // Указатель на результирующий массив
-	c = max_vect(kc, a, b);                // Вызов функции для создания массива
-	for (int i = 0;i < kc; i++)            // Вывод результата.
-		cout << c[i] << " ";
-	cout << endl;
-	delete[]c;                             // Возврат памяти.
 }
