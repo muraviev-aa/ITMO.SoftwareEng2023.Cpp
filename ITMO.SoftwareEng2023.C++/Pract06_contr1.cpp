38470f66086a7d6fcc419c373e1b2e0a13124300
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-13 13:02:19 +0300

    Added Pract06_contr1

---
+++ ITMO.SoftwareEng2023.C++/Pract06_contr1.cpp
@@ -0,0 +1,34 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 6. Работа с файлами
+// Контрольные задания.
+// Задание 1. Запись текста в файл
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+void Example1(const char* filename);
+
+void main()
+{
+    system("chcp 1251");
+    Example1("6_1.txt");
+}
+
+void Example1(const char* filename) // Чтение строк из клавиатуры и запись их в файл
+{
+    ofstream os(filename); 
+    char str[100];
+    cout << "Введите строки стихотворения. Для завершения воода введите ТОЧКУ." << endl;
+
+    do
+    {
+        cin.getline(str, 100);
+        os << str << endl;
+    } 
+    while (*str != '.'); // ТОЧКА - окончание ввода
+
+    os.close();
+}
