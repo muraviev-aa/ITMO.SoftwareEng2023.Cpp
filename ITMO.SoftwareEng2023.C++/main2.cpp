ec76638c782f65c81655f06b6ed5d051bebc6345
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-17 18:13:26 +0300

    Added Pract08_ex4

---
+++ ITMO.SoftwareEng2023.C++/main2.cpp
@@ -0,0 +1,62 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 8. Объявление и реализация класса. 
+// Реализация инкапсуляции. Конструкторы и деструкторы.
+// Упражнение 4. Использование конструктора
+//
+
+/* main1.cpp */
+
+#include <iostream>
+#include <string>
+#include "student1.h"
+using namespace std;
+
+
+int main()
+{
+	
+	string name;
+	string last_name;
+
+	// Ввод имени с клавиатуры
+	cout << "Name: ";
+	getline(cin, name);
+
+	// Ввод фамилии
+	cout << "Last name: ";
+	getline(cin, last_name);
+
+	// Передача параметров конструктору
+	Student* student02 = new Student(name, last_name);
+
+	// Оценки
+	int scores[5] = { 0 };
+
+	// Сумма всех оценок
+	int sum = 0;
+
+	// Ввод промежуточных оценок
+	for (int i = 0; i < 5; ++i) {
+		cout << "Score " << i + 1 << ": ";
+		cin >> scores[i];
+		// суммирование
+		sum += scores[i];
+	}
+
+	// Сохраняем промежуточные оценки в объект класса Student
+	student02->set_scores(scores);
+
+	// Считаем средний балл
+	double average_score = sum / 5.0;
+
+	// Сохраняем средний балл в объект класса Student
+	student02->set_average_score(average_score);
+	// Выводим данные по студенту
+	cout << "Average ball for " << student02->get_name() << " "
+		<< student02->get_last_name() << " is "
+		<< student02->get_average_score() << endl;
+
+	delete student02; // Освобождаем память, занимаемую объектом
+
+	return 0;
+}
