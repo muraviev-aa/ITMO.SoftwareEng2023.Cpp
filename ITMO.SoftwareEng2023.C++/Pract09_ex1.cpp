d1e09ed780ad03b477c3331f5405e2d849949127
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-21 08:54:02 +0300

    Added Pract09_ex1

---
+++ ITMO.SoftwareEng2023.C++/Pract09_ex1.cpp
@@ -0,0 +1,47 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 9. Обработка исключительных операций
+// Упражнение 1. Безопасная функция деления
+//
+
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+class DivideByZeroError
+{
+public:
+	DivideByZeroError() : message ("Деление на нуль") { };
+	void printMessage() const { cout << message << endl; };
+private:
+	string message;
+};
+
+float quotient(int num1, int num2)
+{
+	if (num2 == 0)
+		throw DivideByZeroError();
+	return (float)num1 / num2;
+}
+
+int main()
+{
+	system("chcp 1251");
+	int number1, number2;
+	cout << "Введите два целых числа для расчета их частного:\n"; 
+	cin >> number1; 
+	cin >> number2;
+
+	try
+	{
+		float result = quotient(number1, number2);
+		cout << "Частное равно " << result << endl;
+	}
+	catch(DivideByZeroError& error)
+	{
+		cout << "ОШИБКА: ";
+		error.printMessage();
+		return 1;       // завершение программы при ошибке
+	}
+	return 0;
+}
