0ac3d340e07faf72c3b381542c68f0a6348ac2f4
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-02-24 08:16:23 +0300

    Added Pract10_contr1

---
+++ ITMO.SoftwareEng2023.C++/dot.cpp
@@ -0,0 +1,22 @@
+﻿// ITMO.SoftwareEng2023.C++
+// Практическое занятие 10. Реализация отношений между классами
+// Контрольные задания.
+// Задание 1. Реализация класса Triangle
+//
+
+#include "dot.h"
+#include <cmath>
+
+Dot::Dot()
+{
+	x = 0; y = 0;
+}
+Dot::Dot(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+double Dot::distanceTo(Dot point)
+{
+	return sqrt(pow(point.x - x, 2) + pow(point.y - y, 2));
+}
